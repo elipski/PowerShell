036171076250655c80b093c0bd251044709e5cdd
Edward Lipski <edward.lipski@epicor.com>
2019-08-29 10:53:32 -0400

    Added script to recursively delete idvidual files in given directory.

---
+++ GeneralPowerShellScripts/RecursivelyDeleteIndividualFiles.ps1
@@ -0,0 +1,12 @@
+﻿function Get-Tree($Path,$Include='*') { 
+    @(Get-Item $Path -Include $Include -Force) + 
+        (Get-ChildItem $Path -Recurse -Include $Include -Force) | 
+        sort pspath -Descending -unique
+} 
+
+function Remove-Tree($Path,$Include='*') { 
+    Get-Tree $Path $Include | Remove-Item -force -recurse
+} 
+#From Stackoverflow: https://stackoverflow.com/questions/1752677/how-to-recursively-delete-an-entire-directory-with-powershell-2-0 
+#
+#call: Remove-Tree some_dir
